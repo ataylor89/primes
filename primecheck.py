17d1303ba6856bcfc08b49fe00ded1e9340a590c
Andrew Taylor <rolyata47@gmail.com>
2025-12-21 16:16:54 -0600

    Adding primecheck.py

---
+++ primecheck.py
@@ -0,0 +1,30 @@
+import argparse
+
+def sieve(size):
+    arr = ['P'] * size
+    arr[0] = 'N'
+    arr[1] = 'N'
+    for i in range(2, size):
+        if arr[i] == 'P':
+            j = 2
+            while i * j < size:
+                arr[i * j] = 'C'
+                j += 1
+    return arr
+
+def main():
+    parser = argparse.ArgumentParser(prog='primecheck.py', description='Check if a number is prime')
+    parser.add_argument('input', type=int)
+    args = parser.parse_args()
+    n = args.input
+    if n < 2:
+        print('The input has to be a positive integer greater than 1')
+        return
+    arr = sieve(n + 1)
+    if arr[n] == 'P':
+        print('%d is prime' %n)
+    else:
+        print('%d is composite' %n)
+
+if __name__ == '__main__':
+    main()
