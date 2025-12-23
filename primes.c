8efa1182abde82b5d18c0e4adde4696633482edf
Andrew Taylor <rolyata47@gmail.com>
2025-12-23 15:25:27 -0600

    Improving the logic

--- primes.c
+++ primes.c
@@ -2,19 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* sieve(int size) {
-    char* numbers = malloc(sizeof(char)*size);
-    memset(numbers, 'P', size);
-    numbers[0] = '0';
-    numbers[1] = '1';
+struct Sieve {
+    char* table;
+    int size;
+    int num_primes;
+};
+
+struct Sieve sieve(int size) {
+    struct Sieve s;
+    s.table = malloc(sizeof(char)*size);
+    memset(s.table, 'P', size);
+    s.table[0] = '0';
+    s.table[1] = '1';
+    s.size = size;
+    s.num_primes = size - 2;
     for (int i = 2; i < size; i++) {
-        if (numbers[i] == 'P') {
+        if (s.table[i] == 'P') {
             for (int j = 2; i*j < size; j++) {
-                numbers[i*j] = 'C';       
+                if (s.table[i*j] == 'P') {
+                    s.table[i*j] = 'C';
+                    s.num_primes--;
+                }
             }
         } 
     }
-    return numbers;
+    return s;
+}
+
+int* generate(int n) {
+    struct Sieve s;
+    int size = 10 * n;
+    s = sieve(size);
+    while (s.num_primes < n) {
+        size *= 10;
+        s = sieve(size);
+    }
+    int* primes = malloc(sizeof(int)*n);
+    int counter = 0;
+    for (int i = 0; i < size; i++) {
+        if (s.table[i] == 'P') {
+            primes[counter] = i;
+            counter++;
+        }
+    }
+    return primes;
 }
 
 int main(int argc, char **argv) {
@@ -23,22 +54,17 @@ int main(int argc, char **argv) {
         return 0;
     }
     int n = atoi(argv[1]);
-    int count = 0;
-    int size = 1000;
-    char* numbers = sieve(size);
-    for (int i = 2; count < n; i++) {
-        if (i == size) {
-            size *= 10;
-            numbers = sieve(size);
+    if (n < 1) {
+        printf("The input has to be a positive integer\n");
+        return 0;
+    }
+    int* primes = generate(n);
+    for (int i = 0; i < n; i++) {
+        if (i < n - 1) {
+            printf("%d, ", primes[i]);
         }
-        if (numbers[i] == 'P') {
-            count++;
-            if (count < n) {
-                printf("%d, ", i);
-            }
-            else {
-                printf("%d\n", i);
-            }
+        else {
+            printf("%d\n", primes[i]);
         }
     }
     return 0;
