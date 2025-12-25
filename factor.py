ecba19d365b51c21eed53e4aaeccc10b27ee2cd3
Andrew Taylor <rolyata47@gmail.com>
2025-12-25 14:44:20 -0600

    Improving efficiency

--- factor.py
+++ factor.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 import argparse
+import pickle
+import time
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+default_cache_path = os.path.join(script_dir, 'primetable.pickle')
+
+flags = {'used_cache': False}
 
 def factor(n):
-    arr = sieve(n + 1)
+    assert isinstance(n, int) and n > 1, 'n must be a positive integer greater than 1'
+    cache = load()
+    if cache and len(cache) > 0 and n <= cache[-1]:
+        primes = cache
+        flags['used_cache'] = True
+    else:
+        primes = [i for i, j in enumerate(sieve(n+1)) if j == 'P']
+        flags['used_cache'] = False
     primefac = {}
-    for i in range(2, len(arr)):
-        if arr[i] == 'P' and n % i == 0:
-            primefac[i] = 0
-            k = n
-            while k % i == 0:
-                k /= i
-                primefac[i] += 1
+    for p in primes:
+        if n == 1:
+            break
+        if n % p == 0:
+            primefac[p] = 0
+            while n % p == 0:
+                n /= p
+                primefac[p] += 1
     return primefac
 
 def sieve(size):
@@ -26,15 +42,32 @@ def sieve(size):
                 j += 1
     return arr
 
+def load(path = default_cache_path):
+    if os.path.exists(path):
+        with open(path, 'rb') as file:
+            return pickle.load(file)
+
 def main():
-    parser = argparse.ArgumentParser(prog='primecheck.py', description='Check if a number is prime')
+    parser = argparse.ArgumentParser(prog='factor.py', description='Factor a number')
     parser.add_argument('input', type=int)
+    parser.add_argument('-t', '--time', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
-    if args.input < 2:
+    n = args.input
+    if n < 2:
         print('The input has to be a positive integer greater than 1')
         return
-    primefac = factor(args.input)
+    st = time.time()
+    primefac = factor(n)
+    te = time.time() - st
     print(primefac)
-    
+    if args.time:
+        print('Factored %d in %s seconds' %(n, te))
+    if args.verbose:
+        if flags['used_cache']:
+            print('The prime numbers were loaded from file')
+        else:
+            print('The prime numbers were generated from a prime sieve')
+
 if __name__ == '__main__':
     main()
